7d5ced716996489567022186fe1bf8c7577ca97c
akshat khamparia <akshat.0221@gmail.com>
2026-02-15 13:47:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cron = require('node-cron');
+
+const BOT_TOKEN = process.env.BOT_TOKEN.trim();
+const CHAT_ID = process.env.CHAT_ID.trim();
+const PRODUCT_URL = process.env.PRODUCT_URL.trim();
+const PRODUCT_URL_TEST = process.env.PRODUCT_URL_TEST.trim();
+
+let alreadyNotified = false;
+
+async function checkProduct() {
+  try {
+    console.log('Checking product...');
+    const response = await axios.get(PRODUCT_URL, {
+    // const response = await axios.get(PRODUCT_URL_TEST, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+      },
+    });
+
+    const $ = cheerio.load(response.data);
+
+    //Check for in-stock button
+    const inStock = $('a.update_cart_product').length > 0;
+
+    if (inStock) {
+      console.log('Product is IN STOCK');
+
+      if (!alreadyNotified) {
+        await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+          chat_id: CHAT_ID,
+          text: `🔥 Product Available!\n${PRODUCT_URL}`,
+        });
+
+        console.log('Telegram alert sent');
+        alreadyNotified = true;
+      }
+    } else {
+      console.log('Still out of stock');
+      alreadyNotified = false; // reset so next stock triggers again
+    }
+  } catch (error) {
+    // console.error("Error:", error.message);
+    await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      chat_id: CHAT_ID,
+      text: `Error! \n${error.message}`,
+    });
+  }
+}
+
+console.log('Bot started...');
+
+//Check every 2 minutes
+// cron.schedule("*/2 * * * *", () => {
+//   checkProduct();
+// });
+
+// Run immediately once
+checkProduct();
+
+//------------------------------------------test bot-------------------------------------
+// async function sendTest() {
+//   await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+//     chat_id: CHAT_ID,
+//     text: "Bot is working!"
+//   });
+
+//   console.log("Message sent");
+// }
+
+// sendTest();
